620f598cd6989faf4bef0b21f4f68af4fb0357c8
pengpengpong <91673202+pengpongpong@users.noreply.github.com>
2022-09-30 18:02:19 +0200

    fixes

--- index.js
+++ index.js
@@ -45,38 +45,37 @@ const mouseleave = function(e) {
         .style("stroke", "none")
 }
 
+// chart
 const svg = d3.select("#chart")
             .append("svg")
             .attr("width", width)
             .attr("height", height)
 
-const path = d3.geoPath();
+// const path = d3.geoPath();
 const projection = d3.geoMercator()
             .scale(600)
             .center([15, 55])
             .translate([width / 2, height / 2]);
 
-let data = new Map()
-
-
-
+// color scale
 const colorScale = d3.scaleThreshold()
   .domain([0, 1, 3, 5, 8, 10, 15, 20, 30])
   .range(d3.schemeBlues[9]);
 
+// colorscale for legend
 const colorLog = d3.scaleLog()
                 .domain([1, 3, 5, 8, 10, 15, 20, 30])
-                .range(d3.schemeBlues[9])
-                
+                .range(d3.schemeBlues[9]);
+
+// call data
+let data = new Map()
   Promise.all([
     d3.json("https://raw.githubusercontent.com/leakyMirror/map-of-europe/master/GeoJSON/europe.geojson"),
-    d3.csv("europe_unemp.csv", function(d) {
+    d3.csv("europe_unemp.csv", d => {
         data.set(d["country"], d["rate"])
     })
   ]).then(function(loadData) {
-    let topo = loadData[0]
-    console.log(topo.features)
-    // console.log(data)
+    let topo = loadData[0];
 
     svg.append("g")
         .selectAll("path")
@@ -84,7 +83,7 @@ const colorLog = d3.scaleLog()
         .join("path")
             .attr("d", d3.geoPath()
                 .projection(projection))
-            .attr("fill", function (d) {
+            .attr("fill", d => {
                 d.rate = data.get(d["properties"]["NAME"]) || 0;
                 return colorScale(d.rate)
             })
@@ -93,16 +92,11 @@ const colorLog = d3.scaleLog()
             .on("mouseleave", mouseleave)
   })
 
+// legend
 svg.append("g")
     .attr("class", "legend")
     .attr("transform", `translate(50, 300)`)
-
-// let legend = d3.legendColor()
-//             .labelFormat(d3.format(".2f"))
-//             .labels(d3.legendHelpers.thresholdLabels)
-//             .useClass(true)
-//             .scale(colorScale)
-            
+          
 let legend = d3.legendColor()
             .cells([0, 1, 2, 4, 6, 8, 10, 14, 16, 20, 25, 30])
             .scale(colorLog)
